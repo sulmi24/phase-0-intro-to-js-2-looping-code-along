ae8e5066ad58eeec840717c0160181bd4b8ef240
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 20:12:20 +0200

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, nameevnt) {
+  let store = [];
+  for (let i = 0; i < arr.length; i++) {
+    store.push(`Thank you, ${arr[i]}, for the wonderful ${nameevnt} gift!`);
+  }
+  return store;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(num) {
+  for (let i = num; i > -1; ) {
+    console.log(i--);
+  }
+}
+
+countDown(20);
